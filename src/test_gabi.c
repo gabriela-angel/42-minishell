83701792a8362ba6f814aedd93d9d58527a7110e
gabi <gabrielaangel2003@hotmail.com>
2025-04-14 19:01:00 -0300

    adds header file, make file and utils folder

---
+++ src/test_gabi.c
@@ -0,0 +1,19 @@
+
+#include "minishell.h"
+
+int	main(void)
+{
+	char *ptr;
+	ptr = readline("minishell$");
+	
+	return (0);
+}
+
+if (the token == HEREDOC)
+{
+	ptr = readline(">");
+	while (IT IS NOT THE DELIMITOR)
+	{
+		ptr = readline(">");
+	}
+}
