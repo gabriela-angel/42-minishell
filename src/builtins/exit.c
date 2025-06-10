6991614abb048c340d783e8d151c18455ed0362d
gabi <gabrielaangel2003@hotmail.com>
2025-06-10 16:52:20 -0300

    fixes exit output

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: gangel-a <gangel-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 15:30:19 by acesar-m          #+#    #+#             */
-/*   Updated: 2025/06/08 14:32:05 by gangel-a         ###   ########.fr       */
+/*   Updated: 2025/06/10 16:45:24 by gangel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,21 @@ static int	is_numeric(const char *s)
 
 int	exec_exit(char **args, int last_status)
 {
-	ft_printf_fd(2, "exit\n");
 	if (!args[1])
 		cleanup_and_exit(last_status);
 	if (!is_numeric(args[1]))
 	{
-		ft_printf_fd(2, "minishell: exit: numeric argument required\n");
-		cleanup_and_exit(255);
+		ft_printf_fd(2, "exit: numeric argument required\n");
+		ft_printf_fd(1, "exit\n");
+		cleanup_and_exit(2);
 	}
 	if (args[2])
 	{
-		ft_printf_fd(2, "minishell: exit: too many arguments\n");
-		exit_status(1);
+		ft_printf_fd(2, "exit: too many arguments\n");
+		ft_printf_fd(1, "exit\n");
+		cleanup_and_exit(1);
 	}
+	ft_printf_fd(1, "exit\n");
 	cleanup_and_exit((unsigned char)ft_atoi(args[1]));
 	return (0);
 }
